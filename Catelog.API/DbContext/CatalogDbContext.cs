7ea45bc35b70860a37a7e430590c5dbbd167c28c
raselkobir1 <raselkobir29@gmail.com>
2023-04-08 01:00:42 +0600

    working product repository implement

---
+++ Catelog.API/DbContext/CatalogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MongoRepo.Context;
+
+namespace Catelog.API.DbContext
+{
+    public class CatalogDbContext : ApplicationDbContext
+    {
+        static IConfiguration config = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true).Build();
+        static string conString = config.GetConnectionString("Catalog.API");
+        static string dataBase = config.GetValue<string>("DatabaseName");
+        public CatalogDbContext() : base(conString, dataBase)
+        {
+        }
+    }
+}
--- Catelog.API/Program.cs
+++ Catelog.API/Program.cs
@@ -1,14 +1,20 @@
+using Catelog.API.DbContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Configuration.AddJsonFile("appsettings.json", optional: false);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSingleton<CatalogDbContext>();
 
 var app = builder.Build();
 
+// Get the connection string from appsettings.json
+//string connectionString = builder.Configuration.GetConnectionString("Catalog.API");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
