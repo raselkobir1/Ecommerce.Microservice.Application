f9ce5cedd1e964077fa33a5d642f5ead53979ead
raselkobir1 <raselkobir29@gmail.com>
2023-04-28 20:54:34 +0600

    shopping cart model create

---
+++ Basket.API/Models/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Models
+{
+    public class ShoppingCartItem
+    {
+        public string ProductId { get; set; }
+        public int Quentity { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+        public string ProductName { get; set; }
+    }
+}
