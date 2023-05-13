7164cf5e8fb0adb83c9a7705f830536bf2e07ab4
raselkobir1 <raselkobir29@gmail.com>
2023-05-13 22:27:18 +0600

    agein commit

--- Basket.API/Controllers/BasketController.cs
+++ Basket.API/Controllers/BasketController.cs
@@ -4,7 +4,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using CoreApiResponse;
 using System.Net;
-using Basket.API.Models;
 
 namespace Basket.API.Controllers
 {
@@ -18,6 +17,7 @@ namespace Basket.API.Controllers
             _basketRepository = basketRepository;
         }
         [HttpGet]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<IActionResult> GetBasket(string userName)
         {
             try
