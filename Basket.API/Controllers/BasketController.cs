4e6ea6dcd7bc023891b77e27d4aac46356136d87
raselkobir1 <raselkobir29@gmail.com>
2023-05-13 22:23:15 +0600

    basket api controller implemet

---
+++ Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,60 @@
+﻿using Basket.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CoreApiResponse;
+using System.Net;
+using Basket.API.Models;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]/[Action]")]
+    [ApiController]
+    public class BasketController : BaseController
+    {
+        private readonly IBusketRepository _basketRepository;
+        public BasketController(IBusketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+        [HttpGet]   
+        public async Task<IActionResult> GetBasket(string userName)
+        {
+            try
+            {
+                var basket = await _basketRepository.GetBasket(userName);
+                return CustomResult("Basket data load successfully",basket);
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            try
+            {
+                return CustomResult("Basket modified done.", await _basketRepository.UpdateBasket(basket));
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+        [HttpDelete]
+        [ProducesResponseType(typeof(void), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteBasket(string userName)
+        {
+            try
+            {
+                await _basketRepository.DeleteBasket(userName);
+                return CustomResult("Basket has been deleted.");
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+    }
+}
