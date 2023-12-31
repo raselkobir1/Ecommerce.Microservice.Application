7c1a4f62be3cf00886ec0a32fa0eb5e651c7bb64
Rasel <md.rasel@technonext.com>
2024-01-01 00:10:08 +0600

    ++ fluent validation added

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/CreateOrder/CreateOrderCommandValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.CreateOrder
+{
+    public class CreateOrderCommandValidator: AbstractValidator<CreateOrderCommand> 
+    {
+        public CreateOrderCommandValidator()
+        {
+            RuleFor(x => x.UserName)
+                .NotEmpty().MaximumLength(50)
+                .WithMessage("Please enter user name.")
+                .EmailAddress()
+                .WithMessage("User name should be valid email address");
+
+            RuleFor(x => x.EmailAddress).NotEmpty();
+
+            RuleFor(x=> x.FirstName).NotEmpty().MaximumLength(50);
+            RuleFor(x=> x.LastName).NotEmpty().MaximumLength(50);
+            RuleFor(x=> x.TotalPrice).GreaterThan(0);
+
+        }
+    }
+}
