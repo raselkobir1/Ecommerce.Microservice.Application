b1227d3f24ba4884e09a24a0c66e9d3818dd7169
Rasel <md.rasel@technonext.com>
2024-01-14 20:25:04 +0600

    ++ delete order api done

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Orders.Commands.DeleteOrder
+{
+    internal class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand, bool>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository, IMapper mapper)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+        }
+        public async Task<bool> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            bool isDelete = false;
+            var orders = await _orderRepository.GetAllAsync(x=> x.Id == request.Id);
+            if (orders.Any())
+            {
+                var requestObj = new Order() { Id = request.Id };
+                isDelete = await _orderRepository.DeleteAsync(requestObj);
+            }
+            return isDelete;   
+        }
+    }
+}
