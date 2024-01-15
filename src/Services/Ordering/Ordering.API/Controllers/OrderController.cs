5f1ee0e8657b2b8709e89c40fe14f4224ae2351c
Rasel <md.rasel@technonext.com>
2024-01-15 22:54:58 +0600

    ++ create, update and delete api implement

--- src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
+++ src/Services/Ordering/Ordering.API/Controllers/OrderController.cs
@@ -2,7 +2,11 @@
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Ordering.Application.Features.Orders.Commands.CreateOrder;
+using Ordering.Application.Features.Orders.Commands.DeleteOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
 using Ordering.Application.Features.Orders.Queries.GetOrdersByUserName;
+using Ordering.Domain.Models;
 using System.Net;
 
 namespace Ordering.API.Controllers
@@ -30,6 +34,57 @@ namespace Ordering.API.Controllers
                 return CustomResult(ex.Message, HttpStatusCode.BadRequest);
             }
             
+        }
+        [HttpPost]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> CreateOrder(CreateOrderCommand orderCommand)
+        {
+            try
+            {
+                var isOrderPlaced = await _mediator.Send(orderCommand); 
+                if (isOrderPlaced)
+                    return CustomResult("Order successfully placed");
+                return CustomResult("Order not placed");
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> UpdateOrder(UpdateOrderCommand orderCommand)
+        {
+            try
+            {
+                var isOrderModified = await _mediator.Send(orderCommand);
+                if (isOrderModified)
+                    return CustomResult("Order successfully modified");
+                return CustomResult("Order modified failed");
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+        }
+        [HttpDelete]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DeleteOrder(int orderId) 
+        {
+            try
+            {
+                var isOrderDelete = await _mediator.Send(new DeleteOrderCommand { Id = orderId });
+                if (isOrderDelete)
+                    return CustomResult("Order successfully deleted");
+                return CustomResult("Order deleted failed");
+            }
+            catch (Exception ex)
+            {
+                return CustomResult(ex.Message, HttpStatusCode.BadRequest);
+            }
+
         }
     }
 }
