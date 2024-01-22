2fa4327f0ddb08f0b81836cb10197433a374fb91
Rasel <md.rasel@technonext.com>
2024-01-22 23:33:30 +0600

    ++ working on  event bus configurations

---
+++ src/BuildingBlocks/EventBus.Messages/Common/EventBus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Common
+{
+    public static class EventBus
+    {
+        public const string BasketCheckoutQueue = "basketCheckout-queue";
+    }
+    //all event bus name which we use in Queue name
+}
