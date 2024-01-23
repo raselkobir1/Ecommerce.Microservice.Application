4d6b7462fde591f2c0b1d894772066d5a8357527
Rasel <md.rasel@technonext.com>
2024-01-23 23:00:39 +0600

    ++ working on  rabbirMq configuration

---
+++ src/Services/Basket.API/Models/BasketCheckout.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.API.Models
+{
+    public class BasketCheckout
+    {
+        public string UserName { get; set; }
+        public decimal TotalPrice { get; set; }
+        //billing address
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+        //payment 
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        public string CVV { get; set; }
+        public string Expiration { get; set; }
+        public int PaymentMethod { get; set; }
+
+    }
+}
--- src/Services/Basket.API/Program.cs
+++ src/Services/Basket.API/Program.cs
@@ -1,6 +1,7 @@
 using Basket.API.GrpcServices;
 using Basket.API.Repository;
 using Discount.Grpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +13,14 @@ builder.Services.AddScoped<DiscountGrpcService>();
 
 builder.Services.AddControllers();
 //register grpc client
+// RabbitMQ configuration
+builder.Services.AddMassTransit(config =>
+{
+    config.UsingRabbitMq((ctx, cfg) => 
+    {
+        cfg.Host(builder.Configuration["EventBusSettings:RabbitMqHost"]);
+    });
+});
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(option =>
 {
     option.Address = new Uri(builder.Configuration.GetValue<string>("GrpcSettings:DiscountGrpcUrl"));
